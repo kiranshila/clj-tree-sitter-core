99de470846bdadf65b0763fe3c7a280672d813fe
Kiran Shila <me@kiranshila.com>
2022-11-29 18:19:39 -0800

    Josh fixes

--- test/example.clj
+++ test/example.clj
@@ -1,13 +1,12 @@
 (ns example
   (:require
    [tree-sitter.parser :as p]
-   [tree-sitter.node :as n]
    [tree-sitter.tree :as t]
-   [coffi.mem :as mem]))
+   [tree-sitter.node :as n]))
 
 (def tree
   (let [parser (p/make-parser :json)]
     (->> "[1, null]"
          (p/parse-string parser)
-         #_t/root-node
-         #_n/as-string)))
+         t/root-node
+         n/as-string)))
